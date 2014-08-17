8af135f37e7e0cbad61e9cff50fcbcf09f259533
OopsOutOfMemory <victorshengli@126.com>
2014-08-17 23:40:42 +0800

    last commit

---
+++ .worksheet/src/com.shengli.json.MainCl.scala
@@ -0,0 +1,5 @@
+package com.shengli.json
+
+object MainCl {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(85); 
+  println("Welcome to the Scala worksheet")}
+}
--- .worksheet/src/com.shengli.json.Strategy.scala
+++ .worksheet/src/com.shengli.json.Strategy.scala
@@ -8,7 +8,8 @@ case class Strategy(name:String, expression:String)
 case class FieldDesc(fieldName:String, fieldIndex : Int, strategy : Strategy,  splitor : String, extract : String)
 
 
-object MainClass extends App{ 
+object MainClass extends App{
+   
   
    implicit val formats = DefaultFormats // Brings in default date formats etc.
    val line = "2014-08-10	1	5	f1f3_20	2014-08-10 11:17:58  INFO  item - [game][item][game][evnt]CraftLeveNpcTrade,leve_id=107,handler=EventHandlerType#14 EventHandlerIndex#0 EventHandlerId#917504,actor=Chara#19014409509679630 Entity#268672935,pc_name=培尔贝瑞,x=168.161713,y=8.718561,z=-48.012917,target=Kind#3 BaseId#1001220 LayoutId#2257677 Chara#0 Entity#1073742005,target_name=朱利昂贝尔"
@@ -25,18 +26,11 @@ object MainClass extends App{
    println(fieldString)
    
    val strategy = fieldDesc.strategy.name match {
-     case "regex" => 
+     case "regex" =>
        println("regex strategy")
        val res = RegexUtil.MatchRegex(fieldDesc.strategy.expression,fieldString)
        println(res)
-       val fieldValueArray = res.split(fieldDesc.splitor,-1)
-       val fieldValue = fieldDesc.extract match {
-         case "right" =>
-           fieldValueArray(1)
-         case _ =>
-           None
-       }
-       println("%s value is %s".format(fieldDesc.fieldName, fieldValue))
+       val fieldValue = res.split(fieldDesc.splitor,-1)
      case _ => println("error")
    }
    
