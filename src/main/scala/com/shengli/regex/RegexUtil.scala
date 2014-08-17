ee975ebbdd175011bc3f6b2fbccfc6e99f58a0a4
OopsOutOfMemory <victorshengli@126.com>
2014-08-17 17:35:24 +0800

    add

--- src/main/scala/com/shengli/regex/RegexUtil.scala
+++ src/main/scala/com/shengli/regex/RegexUtil.scala
@@ -9,4 +9,15 @@ object RegexUtil {
 	    case None => false
 	  }
 	}
+	
+	
+	
+	def MatchRegex(regex : String, line : String) : String = {
+	  val pattern = regex.r
+	  val res =  pattern findFirstIn line match {
+	    case Some(matched)=> Some(matched).get
+	    case None => None
+	  }
+	  res.toString()
+	}
 }
