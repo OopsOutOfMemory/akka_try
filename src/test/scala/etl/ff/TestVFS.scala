9c15a6df20554d60c375a6b8267d1cba77f32b29
OopsOutOfMemory <victorshengli@126.com>
2014-08-25 16:12:09 +0800

    commit

---
+++ src/test/scala/etl/ff/TestVFS.scala
@@ -0,0 +1,18 @@
+package etl.ff
+
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.FunSpec
+
+class TestVFS extends FunSpec with ShouldMatchers {
+	describe("text read file with absolute path"){
+	  it("test plain file"){
+	    
+	  }
+	  
+	  
+	  it("test hdfs file"){
+	    
+	  }
+	}
+	
+}
